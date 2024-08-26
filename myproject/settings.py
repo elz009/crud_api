e095cc1a7f4fd500d4cf70a55005e6f9891ccc3a
Elzar <elzarjoldoshev92@gmail.com>
2024-08-27 01:36:27 +0600

    add library

--- myproject/settings.py
+++ myproject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -123,3 +124,78 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+JAZZMIN_SETTINGS = {
+    "site_title": "Admin Dashboard",
+    "site_header": "Admin Portal",
+    "site_brand": "My Project",
+    "site_logo": "images/logo.png",  # Путь к логотипу в STATICFILES
+    "login_logo": None,  # Логотип на странице входа
+    "login_logo_dark": None,  # Логотип для темной темы на странице входа
+    "site_logo_classes": "img-circle",
+    "welcome_sign": "Добро пожаловать в админку!",
+    "copyright": "My Company",
+    "search_model": ["auth.User", "myapp.ModelName"],  # Поиск по моделям
+    "user_avatar": "profile.picture",  # Поле в модели пользователя, которое будет использовано для аватара
+
+    "topmenu_links": [
+        {"name": "Home", "url": "admin:index", "permissions": ["auth.view_user"]},
+        {"name": "Support", "url": "https://support.example.com", "new_window": True},
+        {"model": "auth.User"},
+        {"app": "myapp"},
+    ],
+
+    "usermenu_links": [
+        {"name": "Profile", "url": "profile_url", "icon": "fas fa-user",},
+        {"name": "Logout", "url": "logout", "icon": "fas fa-sign-out-alt",},
+    ],
+
+    "show_sidebar": True,
+    "navigation_expanded": True,
+    "hide_apps": ["sessions"],
+    "hide_models": ["auth.Permission"],
+    "order_with_respect_to": ["auth", "myapp.ModelName"],
+
+    "icons": {
+        "auth": "fas fa-users-cog",
+        "auth.user": "fas fa-user",
+        "auth.Group": "fas fa-users",
+    },
+    "default_icon_parents": "fas fa-chevron-circle-right",
+    "default_icon_children": "fas fa-circle",
+    "related_modal_active": False,
+    "custom_css": None,
+    "custom_js": None,
+    "use_google_fonts_cdn": True,
+    "show_ui_builder": False,
+    "changeform_format": "vertical_tabs",
+    "changeform_format_overrides": {"auth.user": "collapsible", "auth.group": "vertical_tabs"},
+}
+
+JAZZMIN_UI_TWEAKS = {
+    "navbar_small_text": False,
+    "footer_small_text": False,
+    "body_small_text": False,
+    "brand_small_text": False,
+    "brand_colour": False,
+    "accent": "accent-primary",
+    "navbar": "navbar-white navbar-light",
+    "no_navbar_border": False,
+    "sidebar": "sidebar-dark-primary",
+    "sidebar_nav_small_text": False,
+    "sidebar_disable_expand": False,
+    "sidebar_nav_child_indent": False,
+    "sidebar_nav_compact_style": False,
+    "sidebar_nav_legacy_style": False,
+    "sidebar_nav_flat_style": False,
+    "theme": "default",
+    "dark_mode_theme": None,
+    "button_classes": {
+        "primary": "btn-primary",
+        "secondary": "btn-secondary",
+        "info": "btn-info",
+        "warning": "btn-warning",
+        "danger": "btn-danger",
+        "success": "btn-success"
+    }
+}
